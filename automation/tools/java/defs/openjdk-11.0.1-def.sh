faac55c1cab87157a1531a21d80e56f237ef8bae
virgo47 <virgo47@gmail.com>
2019-03-11 23:05:35 +0100

    shasum -a 256 I said

--- automation/tools/java/defs/openjdk-11.0.1-def.sh
+++ automation/tools/java/defs/openjdk-11.0.1-def.sh
@@ -20,5 +20,5 @@ JDK_URL="${!JDK_URL_VAR}"
 JDK_DIR="${!JDK_DIR_VAR}"
 JDK_SUM="${!JDK_SUM_VAR}"
 
-[ "$_OS" = "darwin" ] && CHECKSUM_APP="shasum -r" || CHECKSUM_APP=sha256sum
+[ "$_OS" = "darwin" ] && CHECKSUM_APP="shasum -a 256" || CHECKSUM_APP=sha256sum
 [[ "$JDK_URL" == *.zip ]] && UNPACK_APP=unzip || UNPACK_APP="tar xzvf"
