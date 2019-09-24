c446048c986d5e31c47559a5d3b42bfe484c42c3
virgo47 <virgo47@gmail.com>
2019-09-24 09:16:36 +0200

    zulujdk-13-def.sh added

---
+++ automation/project-scripts/global-tools/java/defs/zulujdk-13-def.sh
@@ -0,0 +1,26 @@
+# msys for Windows+Git, linux for Linux or darwin for OSX
+case "${OSTYPE//[0-9.-]*/}" in
+	msys)
+		ARCHIVE_URL=https://cdn.azul.com/zulu/bin/zulu13.27.9-ca-jdk13-win_x64.zip
+		FINAL_DIR=zulu13.27.9-ca-jdk13-win_x64
+		ARCHIVE_SUM=b73a14b5136dc0239e3e11a6a15768d51ba89422560f046a40653a95e9c208e8
+		ARCHIVE_SUM_APP=sha256sum
+		UNPACK_APP=unzip
+		;;
+
+	linux)
+		ARCHIVE_URL=https://cdn.azul.com/zulu/bin/zulu13.27.9-ca-jdk13-linux_x64.tar.gz
+		FINAL_DIR=zulu13.27.9-ca-jdk13-linux_x64
+		ARCHIVE_SUM=539489cb9309941ca940c2c1b3227bb10ad7a788eb89e663a6dc0d8def6efe86
+		ARCHIVE_SUM_APP=sha256sum
+		UNPACK_APP="tar xzvf"
+		;;
+
+	darwin)
+		ARCHIVE_URL=https://cdn.azul.com/zulu/bin/zulu13.27.9-ca-jdk13-macosx_x64.tar.gz
+		FINAL_DIR=zulu13.27.9-ca-jdk13-macosx_x64
+		ARCHIVE_SUM=620c02559836ebbe42a8ca17e27d5f25261523b5af364cc4c32f21c57398e287
+		ARCHIVE_SUM_APP="shasum -a 256"
+		UNPACK_APP="tar xzvf"
+		;;
+esac
